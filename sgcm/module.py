0f7cedeb4e2342834b355b1375140301f32f2370
Squirrel <squirrelhomebrew@gmail.com>
2025-03-29 01:27:29 -0700

    Update module.py

--- sgcm/module.py
+++ sgcm/module.py
@@ -2,11 +2,18 @@ import os
 import platform
 import json
 import shutil
-import datetime
 import base64
 import requests
 import urllib.parse
 import subprocess
+import zipfile
+import tempfile
+import threading
+import sys
+import time
+import inquirer
+import socket
+from PIL import Image
 
 # variables
 MORSE_CODE_DICT = {
@@ -72,7 +79,8 @@ MORSE_CODE_DICT = {
 # file operations and more
 def write(file_name, mode, content):
     try:
-        with open(file_name, mode) as file:
+        full_path = os.path.join(os.getcwd(), file_name)  
+        with open(full_path, mode) as file:
             if isinstance(content, dict) or isinstance(content, list):
                 content = json.dumps(content, indent=4)
             file.write(content)
@@ -81,56 +89,80 @@ def write(file_name, mode, content):
         print(f"\033[31m[ERROR] Failed to write to file: {e}\033[0m")
         return False
 
-
 def read(file_name):
     try:
-        with open(file_name, "r") as file:
+        full_path = os.path.join(os.getcwd(), file_name)  
+        with open(full_path, "r") as file:
             return file.read()
     except IOError as e:
         print(f"\033[31m[ERROR] Failed to read file: {e}\033[0m")
         return ""
 
-
 def append(file_name, content):
     return write(file_name, "a", content)
 
-
 def fileExists(file_name):
-    return os.path.exists(file_name)
-
+    full_path = os.path.join(os.getcwd(), file_name)  
+    return os.path.exists(full_path)
 
 def fileSize(file_name):
     try:
-        return os.path.getsize(file_name)
+        full_path = os.path.join(os.getcwd(), file_name)  
+        return os.path.getsize(full_path)
     except OSError as e:
         print(f"\033[31m[ERROR] Failed to get file size: {e}\033[0m")
         return -1
 
-
 def deleteFile(file_name):
     try:
-        os.remove(file_name)
+        full_path = os.path.join(os.getcwd(), file_name)  
+        os.remove(full_path)
         return True
     except OSError as e:
         print(f"\033[31m[ERROR] Failed to delete file: {e}\033[0m")
         return False
 
-
 def rename(old_name, new_name):
     try:
-        os.rename(old_name, new_name)
+        old_full_path = os.path.join(os.getcwd(), old_name) 
+        new_full_path = os.path.join(os.getcwd(), new_name) 
+        os.rename(old_full_path, new_full_path)
         return True
     except OSError as e:
         print(f"\033[31m[ERROR] Failed to rename file: {e}\033[0m")
         return False
 
-
 def fileCopy(src, dest):
     try:
-        shutil.copy(src, dest)
+        src_full_path = os.path.join(os.getcwd(), src) 
+        dest_full_path = os.path.join(os.getcwd(), dest) 
+        shutil.copy(src_full_path, dest_full_path)
         return True
     except Exception as e:
         print(f"\033[31m[ERROR] Failed to copy file: {e}\033[0m")
+        return False
+
+def zip_files(file_list, zip_file_name):
+    try:
+        zip_full_path = os.path.join(os.getcwd(), zip_file_name) 
+        with zipfile.ZipFile(zip_full_path, 'w') as zipf:
+            for file in file_list:
+                file_full_path = os.path.join(os.getcwd(), file) 
+                zipf.write(file_full_path, os.path.basename(file))
+        return True
+    except Exception as e:
+        print(f"\033[31m[ERROR] Failed to zip files: {e}\033[0m")
+        return False
+
+def unzip_files(zip_file_name, extract_to):
+    try:
+        zip_full_path = os.path.join(os.getcwd(), zip_file_name) 
+        extract_full_path = os.path.join(os.getcwd(), extract_to) 
+        with zipfile.ZipFile(zip_full_path, 'r') as zipf:
+            zipf.extractall(extract_full_path)
+        return True
+    except Exception as e:
+        print(f"\033[31m[ERROR] Failed to unzip files: {e}\033[0m")
         return False
 
 
@@ -172,7 +204,30 @@ def command(cmd):
         print(f"\033[31m[ERROR] Failed to execute command: {e}\033[0m")
 
 def playAudio(file_path: str) -> None:
-    subprocess.run(["ffplay", "-nodisp", "-autoexit", file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    try:
+        if file_path.startswith("http://") or file_path.startswith("https://"):
+            response = requests.get(file_path)
+            response.raise_for_status()
+
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
+                temp_audio.write(response.content)
+                temp_file_name = temp_audio.name
+
+            subprocess.run(["ffplay", "-nodisp", "-autoexit", temp_file_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            os.unlink(temp_file_name)
+        else:
+            subprocess.run(["ffplay", "-nodisp", "-autoexit", file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except Exception as e:
+        print(f"\033[31m[ERROR] Failed to play audio: {e}\033[0m")
+
+
+def getIp():
+    try:
+        local_ip = socket.gethostbyname(socket.gethostname())
+        public_ip = requests.get('https://api64.ipify.org?format=json').json()["ip"]
+        return {"Local IP": local_ip, "Public IP": public_ip}
+    except Exception as e:
+        return {"Error": f"Failed to get IP: {e}"}
 
 
 # string functions
@@ -219,27 +274,28 @@ def jsonRead(file_name):
         return {}
 
 
-def list_directory(directory):
-    try:
-        return os.listdir(directory)
-    except OSError as e:
-        print(f"\033[31m[ERROR] Failed to list directory: {e}\033[0m")
-        return []
+
 
 
 # dictionary stuff
 def dictionaryCreate(directory):
     try:
-        os.makedirs(directory, exist_ok=True)
+        full_path = os.path.join(os.getcwd(), directory) 
+        os.makedirs(full_path, exist_ok=True)
         return True
     except OSError as e:
         print(f"\033[31m[ERROR] Failed to create directory: {e}\033[0m")
         return False
-
-
+def list_directory(directory):
+    try:
+        full_path = os.path.join(os.getcwd(), directory)  
+        return os.listdir(full_path)
+    except OSError as e:
+        print(f"\033[31m[ERROR] Failed to list directory: {e}\033[0m")
+        return []
 def directoryExists(directory):
-    return os.path.isdir(directory)
-
+    full_path = os.path.join(os.getcwd(), directory) 
+    return os.path.isdir(full_path)
 
 # decode and encode random stuff
 def morseEncode(text):
@@ -307,3 +363,130 @@ def urlEncode(text):
 
 def urlDecode(encoded_text):
     return urllib.parse.unquote(encoded_text)
+
+def webDownload(url, destination):
+    try:
+        full_dest_path = os.path.join(os.getcwd(), destination) 
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024
+        downloaded = 0
+        animation_chars = ['\\', '|', '/', '-']
+        animation_index = 0
+
+        with open(full_dest_path, 'wb') as file:
+            for data in response.iter_content(block_size):
+                downloaded += len(data)
+                file.write(data)
+                percentage = (downloaded / total_size) * 100 if total_size else 0
+                print(f"\rDownloading: {percentage:.2f}% {animation_chars[animation_index % len(animation_chars)]}", end="")
+                animation_index += 1
+        print("\nDownload complete!")
+        return True
+    except Exception as e:
+        print(f"\n\033[31m[ERROR] Download failed: {e}\033[0m")
+        return False
+
+def ferdinhaUpload(file_path=None):
+    url = "https://feridinha.com/upload"
+
+    def choose_file():
+        media_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.mp4', '.mp3', '.wav', '.ogg', '.flv', '.mkv', 'webp', '.avi', '.mov', '.wmv']
+        file_choices = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f)) and any(f.lower().endswith(ext) for ext in media_extensions)]
+        if not file_choices:
+            print("No media files found...")
+            return None
+        questions = [
+            inquirer.List('file', message="Select a media file to upload", choices=file_choices),
+        ]
+        answers = inquirer.prompt(questions)
+        if answers is None:
+            print("Cancelled.")
+            return None
+        return answers['file']
+
+    def loading_spinner():
+        spinner = ['|', '/', '-', '\\', '◜', '◞', '◝', '◞', '◟', '◠', '◡']
+        while True:
+            for symbol in spinner:
+                sys.stdout.write(f'\rUploading {symbol}       ')
+                sys.stdout.flush()
+                time.sleep(0.1)
+
+    if file_path is None:
+        file_path = choose_file()
+        if file_path is None:
+            return
+
+    full_file_path = os.path.join(os.getcwd(), file_path)
+
+    if not os.path.isfile(full_file_path):
+        print(f"File '{file_path}' not found.")
+        return
+    
+    spinner_thread = threading.Thread(target=loading_spinner)
+    spinner_thread.daemon = True 
+    spinner_thread.start()
+
+    try:
+        with open(full_file_path, 'rb') as f:
+            files = {'file': f}
+            response = requests.post(url, files=files)
+            if response.status_code == 200:
+                print("\rSuccess!                     ")
+                response_data = response.json()
+                print("File URL:", response_data['message'])
+            else:
+                print(f"\rFailed, error: {response.status_code}                     ")
+    except Exception as e:
+        print(f"\rFailed, error: {e}                     ")
+
+def lastFM(api_key, username):
+    BASE_URL = "https://ws.audioscrobbler.com/2.0/"
+
+    def fetch_recent_tracks():
+        try:
+            params = {
+                'method': 'user.getRecentTracks',
+                'api_key': api_key,
+                'user': username,
+                'limit': 5,
+                'format': 'json'
+            }
+
+            response = requests.get(BASE_URL, params=params)
+            response.raise_for_status()
+
+            data = response.json()
+            recent_tracks = data.get('recenttracks', {}).get('track', [])
+            return recent_tracks
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching recent tracks: {e}")
+            return None
+        except ValueError as e:
+            print(f"Error decoding JSON response: {e}")
+            return None
+        
+
+    return fetch_recent_tracks()
+
+        
+# image stuff
+def imageOpen(image_path):
+    try:
+        img = Image.open(image_path)
+        img.show()
+        return img
+    except Exception as e:
+        print(f"\033[31m[ERROR] Failed to open image: {e}\033[0m")
+        return None
+
+def imageResize(img, width, height):
+    try:
+        resized_img = img.resize((width, height))
+        resized_img.show()
+        return resized_img
+    except Exception as e:
+        print(f"\033[31m[ERROR] Failed to resize image: {e}\033[0m")
+        return None
